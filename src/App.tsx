208f6d9e30d44787b16fbc932943afd3a2a67b15
Vitor <vitorsgomes@gmail.com>
2020-09-19 14:09:11 -0300

    feat: adds initial styling using styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #F8F8F8;
+    font-family: Helvetica, sans-serif;
+  }
+  
+  a {
+    color: inherit;
+    text-decoration: inherit;
+  }
+`;
+
+const MainWrapper = styled.section`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 10px;
+`;
 
 function App() {
-  return <h1>Movies list</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <MainWrapper>
+        <h1>Movies list</h1>
+      </MainWrapper>
+    </>
+  );
 }
 
 export default App;
